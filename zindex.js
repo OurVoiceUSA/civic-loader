c99c1f40f8764601583433d9409e8744159c7d66
Corey Henderson <corman@cormander.com>
2018-03-31 16:54:51 -0600

    cleaner index name, and index source too

--- zindex.js
+++ zindex.js
@@ -19,11 +19,13 @@ function cleanobj(obj) {
   }
 }
 
-async function indexObj(obj, id) {
+async function indexObj(obj, id, key) {
   if (obj == null) return;
   Object.getOwnPropertyNames(obj).forEach((prop) => {
-    rc.sadd('zindex:'+obj[prop], id);
+    let val = obj[prop].replace(/(?:\r\n|\r|\n|\t| |"|\\|)/g, '').toLowerCase();
+    rc.sadd('zindex:'+val, id);
   });
+  if (key) rc.sadd('zindex:'+key, id);
 }
 
 // redis connection
@@ -40,25 +42,32 @@ var rc = redis.createClient(ovi_config.redis_port, ovi_config.redis_host,
 rc.on('connect', async function() {
   console.log('Connected to redis at host "'+ovi_config.redis_host+'"');
 
+  let keys = await rc.keysAsync('zindex:*');
+  for (let k in keys)
+    rc.del(keys[k]);
+
   let pols = await rc.keysAsync('politician:*');
 
   for (let pid in pols) {
     let politician_id = pols[pid].split(":")[1];
 
     let pol = await rc.hgetallAsync('politician:'+politician_id);
-    indexObj(pol, politician_id);
+    indexObj(pol, politician_id, null);
 
     let fec = await rc.hgetallAsync('fec:'+pol.fec_candidate_id);
-    indexObj(fec, politician_id);
+    indexObj(fec, politician_id, 'fec');
 
     let uslc = await rc.hgetallAsync('uslc:'+pol.uslc_id);
-    indexObj(uslc, politician_id);
+    indexObj(uslc, politician_id, 'uslc');
 
     let ep = await rc.hgetallAsync('everypolitician:'+pol.everypolitician_id);
-    indexObj(ep, politician_id);
+    indexObj(ep, politician_id, 'everypolitician');
 
     let os = await rc.hgetallAsync('openstates:'+pol.openstates_id);
-    indexObj(os, politician_id);
+    indexObj(os, politician_id, 'openstates');
+
+    let cfar = await rc.hgetallAsync('cfar:'+politician_id);
+    indexObj(cfar, politician_id, 'cfar');
   }
 
   process.exit(0);
